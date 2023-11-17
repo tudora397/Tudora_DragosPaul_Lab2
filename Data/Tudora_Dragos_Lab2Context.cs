c5c67f4e6879f09c0bc70d21be46ce525dbca355
MB <MB@DESKTOP-6L4TDBT>
2023-11-17 15:23:09 +0200

    Add project files.

---
+++ Data/Tudora_Dragos_Lab2Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Tudora_Dragos_Lab2.Models;
+
+namespace Tudora_Dragos_Lab2.Data
+{
+    public class Tudora_Dragos_Lab2Context : DbContext
+    {
+        public Tudora_Dragos_Lab2Context (DbContextOptions<Tudora_Dragos_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Tudora_Dragos_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Tudora_Dragos_Lab2.Models.Publisher>? Publisher { get; set; }
+    }
+}
---
+++ Pages/Books/Index.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Tudora_Dragos_Lab2.Data;
+using Tudora_Dragos_Lab2.Models;
+
+namespace Tudora_Dragos_Lab2.Pages.Books
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Tudora_Dragos_Lab2.Data.Tudora_Dragos_Lab2Context _context;
+
+        public IndexModel(Tudora_Dragos_Lab2.Data.Tudora_Dragos_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Book> Book { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Book != null)
+            {
+                Book = await _context.Book
+                    .Include(b => b.Publisher)
+                    .ToListAsync();
+            }
+        }
+    }
+}
