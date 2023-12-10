0ad576bac228f3b105daf10ef53fa5b08dfb6b81
DRAGOȘ-PAUL TUDORA <dragos.tudora@stud.ubbcluj.ro>
2023-12-10 18:44:19 +0200

    New

---
+++ Models/ViewModels/PublisherIndexData.cs
@@ -0,0 +1,8 @@
+﻿namespace Tudora_Dragos_Lab2.Models.ViewModels
+{
+    public class PublisherIndexData
+    {
+        public IEnumerable<Publisher> Publishers { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+    }
+}
